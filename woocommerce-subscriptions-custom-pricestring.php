2d7f63630dd97be4ac187ca85a0db96784925fb4
Joey Ricketts <19918755+jrick1229@users.noreply.github.com>
2018-08-06 14:27:20 -0400

    Update woocommerce-subscriptions-custom-pricestring.php
    Update WC version requirements

--- woocommerce-subscriptions-custom-pricestring.php
+++ woocommerce-subscriptions-custom-pricestring.php
@@ -7,8 +7,8 @@
  * Author URI: https://prospress.com/
  * License: GPLv3
  * Version: 1.0.0
- * Requires at least: 4.0
- * Tested up to: 4.8
+ * WC requires at least: 3.0.0
+ * WC tested up to: 3.4.0
  *
  * GitHub Plugin URI: Prospress/woocommerce-subscriptions-custom-pricestring
  * GitHub Branch: master
